2edb63a56f2528b7ddb0814ca49411b850a49d89
omer canpolat <114859630+omercanpolat@users.noreply.github.com>
2023-03-02 12:20:28 +0300

    second push

--- rc11-12_router-example/rc11-12_router-example-inClass/src/pages/PrivateRouter.jsx
+++ rc11-12_router-example/rc11-12_router-example-inClass/src/pages/PrivateRouter.jsx
@@ -1,12 +1,13 @@
-import { Navigate, Outlet } from "react-router-dom"
+import { Navigate, Outlet } from "react-router-dom";
 
 //? Bu kisim gercekte Global State'lerde tutulur. (Context API, Redux, Mobx)
 //! const isSigned = userContext(jwt)
 
-const isSigned = true
+const isSigned = true;
 
 const PrivateRouter = () => {
-  return <div>{isSigned ? <Outlet /> : <Navigate to="/login" />}</div>
-}
+  return <div>{isSigned ? <Outlet /> : <Navigate to="/login" />}</div>;
+};
+// deneme
 
-export default PrivateRouter
+export default PrivateRouter;
